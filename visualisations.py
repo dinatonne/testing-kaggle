890760dc90478c728d6de2fda920e1c70bba4424
dinatonne <293829567+dinatonne@users.noreply.github.com>
2026-06-22 12:20:05 +0200

    Creating plots to visualise aspects of the dataset

---
+++ visualisations.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df.read_csv('tallest_people_in_the_world.csv')
+
+df['height_cm'].hist()
+plt.title('Height Distribution')
+plt.show()
+
+df.groupby('continent')['lifespan'].mean().plot(kind=bar)
+plt.title('Average Lifespan by Continent')
+plt.show()
+
+df.plot.scatter(x='height_cm', y='lifespan')
+plt.title('Height vs. Lifespan')
+plt.show()
