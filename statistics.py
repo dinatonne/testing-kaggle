28a8f512989616db20ef05adf390d5ed64bb6679
dinatonne <293829567+dinatonne@users.noreply.github.com>
2026-06-22 14:28:49 +0200

    Fixed bugs and mistakes in code.

--- statistics.py
+++ statistics.py
@@ -6,27 +6,27 @@ df = pd.read_csv('tallest_people_in_the_world.csv')
 print("Summary statistics by continent:")
 print(df.groupby('continent')[['height_cm', 'lifespan', 'weight_kg']].describe())
 
-print('n\Corrolations:')
+print('\nCorrolations:')
 print(df[['height_cm', 'lifespan', 'weight_kg']].corr())
 
-print('n\Outliers:')
+print('\nOutliers:')
 mean_height = df['height_cm'].mean()
 std_height = df['height_cm'].std() 
 
 outliers = df[df['height_cm'] > mean_height + 2 * std_height] # Formel 
-print('n\Height outlier:')
+print('\nHeight outlier:')
 print(outliers[['name', 'country', 'height_cm']])
 
-print('n\Mean vs Median height by continent:')
+print('\nMean vs Median height by continent:')
 print(df.groupby('continent')['height_cm'].agg(['mean', 'median']))
 
 # Tallest and shortest person by continent
-print('n\Tallest person per continent:')
-print(df.loc[df.groupby('continent')['height_cm'].idmax()][['name', 'continent', 'height_cm']])
+print('\nTallest person per continent:')
+print(df.loc[df.groupby('continent')['height_cm'].idxmax()][['name', 'continent', 'height_cm']])
 
-print('n\Shortest person per continent:')
-print(df.loc[df.groupby('continent')['height_cm'].idmin()][['name', 'continent', 'height_cm']])
+print('\nShortest person per continent:')
+print(df.loc[df.groupby('continent')['height_cm'].idxmin()][['name', 'continent', 'height_cm']])
 
 # Heaviest person per continent
 print('\nHeaviest person per continent:')
-print(df.loc[df.groupby('continent')['weight_kg'].idmax()][['name', 'continent', 'weight_kg']])
+print(df.dropna(subset=['weight_kg']).loc[df.dropna(subset=['weight_kg']).groupby('continent')['weight_kg'].idxmax()][['name', 'continent', 'weight_kg']])
